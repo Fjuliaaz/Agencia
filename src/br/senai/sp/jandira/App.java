827075c494f8dbc303b0749d3670afbd13daf4e8
JULIA APARECIDA DA SILVA FONSECA <23111808@CFP127ALUNO.LOCAL>
2023-09-27 12:17:27 -0300

    finalizado

---
+++ src/br/senai/sp/jandira/App.java
@@ -0,0 +1,16 @@
+package br.senai.sp.jandira;
+
+import br.senai.sp.jandira.model.Menu;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("/// -----  Bem Vindo ao Banco do Senai  ------  ///");
+
+        Menu menu = new Menu();
+        menu.executarMenu();
+
+    }
+}
